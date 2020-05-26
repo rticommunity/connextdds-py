f4a63987cd08b00f83adc9bb4b7ff579dae4a7c8
hman523 <shbarbella@gmail.com>
2020-05-26 17:40:26 -0400

    Added sanity test

---
+++ test/python/test_sanity.py
@@ -0,0 +1,11 @@
+"""
+Test Sanity
+This test is to ensure that the testing framework itself is working
+"""
+
+class TestSanity:
+    def test_one_plus_one(self):
+        """
+        This test ensures 1+1=2
+        """
+        assert 1+1==2
