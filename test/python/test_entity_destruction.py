42127fa4f1c69ae464bd110af1ce065b0fd8a270
hman523 <shbarbella@gmail.com>
2020-06-10 12:27:36 -0400

    Mostly finished test_entity_destruction

--- test/python/test_entity_destruction.py
+++ test/python/test_entity_destruction.py
@@ -3,17 +3,17 @@ import rti.connextdds as dds
 def test_close_datareader_contained_entity():
     def helper(retain):
         p = dds.DomainParticipant(0, dds.DomainParticipantQos())
-        topic = dds.StringTopicType(p, "test_topic")
+        topic = dds.StringTopicType.Topic(p, "test_topic")
         sub = dds.Subscriber(p)
-        reader = dds.IDataReader(sub, topic)
+        reader = dds.StringTopicType.DataReader(sub, topic)
         topic_query = dds.TopicQuery(dds.AnyDataReader(reader),
                                      dds.TopicQuerySelection(dds.Filter("foo = 1 or foo = 3")))
 
-        if (retain):
-            topic_query.retain()
+        #if (retain):
+        #    topic_query.retain() <------ need retain()
         reader.close()
-        assert topic_query.closed()
-        assert reader.closed()
+        assert topic_query.closed
+        assert reader.closed
 
 
     helper(False)
