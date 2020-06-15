b2db81798118de83cd9f5cdaff1f025c678575a8
hman523 <shbarbella@gmail.com>
2020-06-15 10:31:50 -0400

    A few more qos provider tests

--- test/python/test_qos_provider.py
+++ test/python/test_qos_provider.py
@@ -1,5 +1,5 @@
 import rti.connextdds as dds
-
+import pytest
 
 def test_string_uri_creation():
     assert (
@@ -12,12 +12,31 @@ def test_file_uri_creation():
 
 
 def test_invalid_creation():
-    pass
-
+    with pytest.raises(dds.Error):
+        dds.QosProvider("invalid_file.xml")
+    with pytest.raises(dds.Error):
+        dds.QosProvider("../xml/InvalidXml.xml")
 
 def test_default_provider():
-    pass
-
+    provider1 = dds.QosProvider.default()
+    provider2 = dds.QosProvider.default()
+    provider3 = provider2
+    provider4 = provider3
+    assert 3 == len(provider1.qos_profile_libraries())
+    assert 3 == len(provider2.qos_profile_libraries())
+    assert 3 == len(provider3.qos_profile_libraries())
+    assert 3 == len(provider4.qos_profile_libraries())
+    
+    params = dds.QosProviderParams()
+    params.url_profile = dds.StringSeq(["../xml/XmlApplication.xml"])
+    provider4.reload_profiles()
+    provider4.provider_params = params
+    
+    assert 4 == len(provider1.qos_profile_libraries())
+    assert 4 == len(provider2.qos_profile_libraries())
+    assert 4 == len(provider3.qos_profile_libraries())
+    assert 4 == len(provider4.qos_profile_libraries())
+    
 
 def test_getters():
     pass
