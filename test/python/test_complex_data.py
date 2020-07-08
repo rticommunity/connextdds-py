8e77bc6fba8f6e70243e0e9640fdf72f88cd0ba5
hman523 <shbarbella@gmail.com>
2020-07-08 11:35:34 -0400

    Made some changes, tests aren't passing yet

--- test/python/test_complex_data.py
+++ test/python/test_complex_data.py
@@ -32,18 +32,56 @@ def test_nested_struct():
     nested_type.add_member(dds.Member("a", NON_PRIMITIVE_TYPE))
     nested_type.add_member(dds.Member("b", NON_PRIMITIVE_TYPE))
     sample = dds.DynamicData(nested_type)
-    
+
     # Fill in the values
     sample["a.x"] = 1
     sample["a.y"] = 2
     sample["b.x"] = 3
     sample["b.y"] = 4
-    
+
     # Assert they are correct
     assert sample["a.x"] == 1
     assert sample["a.y"] == 2
     assert sample["b.x"] == 3
     assert sample["b.y"] == 4
+
+
+def test_nested_struct_copy():
+    nested_type = dds.StructType("nested_type")
+    nested_type.add_member(dds.Member("a", NON_PRIMITIVE_TYPE))
+    nested_type.add_member(dds.Member("b", NON_PRIMITIVE_TYPE))
+    sample = dds.DynamicData(nested_type)
+
+    # Fill in the values
+    sample["a.x"] = 1
+    sample["a.y"] = 2
+    sample["b.x"] = 3
+    sample["b.y"] = 4
+
+    nested = sample["a"]
+    x = nested["x"]
+    nested["y"] = 23432
+    sample["a"] = nested
+    assert nested["y"] == 23432
+    assert nested["x"] == x
+
+
+def test_nested_struct_loan():
+    nested_type = dds.StructType("nested_type")
+    nested_type.add_member(dds.Member("a", NON_PRIMITIVE_TYPE))
+    nested_type.add_member(dds.Member("b", NON_PRIMITIVE_TYPE))
+    sample = dds.DynamicData(nested_type)
+
+    # Fill in the values
+    sample["a.x"] = 1
+    sample["a.y"] = 2
+    sample["b.x"] = 3
+    sample["b.y"] = 4
+
+    loaned_nested = sample.loan_value("a")
+    loaned_nested.data["x"] = 234234
+    loaned_nested.return_loan()
+    assert sample["a.x"] == 234234
 
 
 @pytest.mark.parametrize("size", [1, 4, 16])
@@ -65,4 +103,17 @@ def test_squence_of_nested_structs(size):
     for i in range(0, size):
         assert sample[i] == dds.DynamicData(
             nested_type, {"a": {"x": i, "y": i + 1}, "b": {"x": i + 2, "y": i + 3}}
+        )
+
+    # Assign values via a loan
+    for i in range(0, size):
+        loaned = sample[i].loan_value("a")
+        loaned.data["x"] = -1 * i
+        loaned.return_loan()
+
+    # Assert correct values
+    for i in range(0, size):
+        assert sample[i] == dds.DynamicData(
+            nested_type,
+            {"a": {"x": (-1 * i), "y": i + 1}, "b": {"x": i + 2, "y": i + 3}},
         )
