a81faa9e9d39955abfab8fbe5bfb1ba2e12c8c67
hman523 <shbarbella@gmail.com>
2020-07-10 12:12:25 -0400

    Added a second test and fixed the first one

--- test/python/perf.py
+++ test/python/perf.py
@@ -3,53 +3,81 @@ import time
 import pathlib
 import argparse
 import sys
-import utils
 
 FILE = str(pathlib.Path(__file__).parent.absolute()) + "/../xml/PerfTest.xml"
 
 
-def main(count):
+def main(count, ind_test, all_test):
     if count <= 0:
         print("Count cannot be zero or below")
         sys.exit(1)
-    print(f"Running test {count} times")
+    print(f"Running test {count} time" + ("s" if count > 1 else ""))
 
     # By setting these to really high and low values, it ensures that we will have a
     # proper min and max value at the end
-    min_time = float("inf")
-    max_time = float("-inf")
+    min_time_all = float("inf")
+    max_time_all = float("-inf")
 
-    total_time = 0
+    min_time_ind = float("inf")
+    max_time_ind = float("-inf")
 
-    system = utils.TestSystem(0, "PerformanceTest")
+    total_time_all = 0
+    total_time_ind = 0
+
     perf_test_type = dds.QosProvider(FILE).type("PerformanceTest")
     sample = dds.DynamicData(perf_test_type)
     for i in range(0, count):
 
-        loan = sample.loan_value("myOctSeq")
-        loan.data[i] = 25
-        loan.return_loan()
-        system.writer.write(sample)
-        recieved = system.reader.take()
+        if all_test:
+            # For testing assigning to a whole array at once
+            start_all = time.time()
+            sample["myOctSeq"] = [25] * 600000
+            lst = sample["myOctSeq"]
 
-        start = time.time()
-        for s in recieved:
-            dictionary = s.data
-        outArray = sample["myOctSeq"]
+            elapsed_all = time.time() - start_all
 
-        elapsed = time.time() - start
+            min_time_all = min(min_time_all, elapsed_all)
+            max_time_all = max(max_time_all, elapsed_all)
+            total_time_all += elapsed_all
 
-        min_time = min(min_time, elapsed)
-        max_time = max(max_time, elapsed)
-        total_time += elapsed
+        if ind_test:
+            # For testing assigning to an array element by element
+            start_ind = time.time()
+            loan = sample.loan_value("myOctSeq")
+            for j in range(0, 600000):
+                loan.data[j] = 25
+                x = loan.data[j]
+            loan.return_loan()
 
-    average = total_time / count
+            elasped_ind = time.time() - start_ind
 
-    print(f"Average: {average}\nMinimum: {min_time}\nMaximum: {max_time}")
+            min_time_ind = min(min_time_ind, elasped_ind)
+            max_time_ind = max(max_time_ind, elasped_ind)
+            total_time_ind += elasped_ind
+
+    if all_test:
+        print("\tAssigning all at once")
+        average_all = total_time_all / count
+        print(
+            f"Average: {average_all}\nMinimum: {min_time_all}\nMaximum: {max_time_all}\nTotal:   {total_time_all}"
+        )
+    if ind_test:
+        print("\tAssigning individually")
+        average_ind = total_time_ind / count
+        print(
+            f"Average: {average_ind}\nMinimum: {min_time_ind}\nMaximum: {max_time_ind}\nTotal:   {total_time_ind}"
+        )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("count", type=int, nargs="?", default=1000)
-    arg = parser.parse_args()
-    main(arg.count)
+    parser.add_argument("-all", action="store_true")
+    parser.add_argument("-ind", action="store_true")
+    args = parser.parse_args()
+    if args.all == args.ind:
+        main(args.count, True, True)
+    elif args.all:
+        main(args.count, False, True)
+    elif args.ind:
+        main(args.count, True, False)
