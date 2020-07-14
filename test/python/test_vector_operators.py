796107d67c535731bb62297ec7b7e7b0a6a891bd
hman523 <shbarbella@gmail.com>
2020-07-14 12:56:58 -0400

    Started test

---
+++ test/python/test_vector_operators.py
@@ -0,0 +1,14 @@
+import rti.connextdds as dds
+
+
+test_addition():
+    pass
+
+test_addition_assignment():
+    pass
+
+test_multiplication():
+    pass
+
+test_multiplication_assignment():
+    pass
