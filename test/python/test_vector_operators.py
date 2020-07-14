c358577c75a1a4bc183b9f5d691e4d956c203199
hman523 <shbarbella@gmail.com>
2020-07-14 13:28:00 -0400

    Added a few tests

--- test/python/test_vector_operators.py
+++ test/python/test_vector_operators.py
@@ -1,14 +1,32 @@
 import rti.connextdds as dds
 
 
-test_addition():
+
+
+
+def get_seq_type(size):
+    return dds.SequenceType(dds.Int32Type(), size)
+
+def test_addition():
+    s1 = dds.DynamicData(get_seq_type(4))
+    s2 = dds.DynamicData(get_seq_type(4))
+    s1 = list(range(0,4))
+    s2 = list(range(4,8))
+    s3 = s1 + s2
+    for i in range(0, 8):
+        assert s3[i] == i
+
+def test_addition_assignment():
+    s1 = dds.DynamicData(get_seq_type(4))
+    s2 = dds.DynamicData(get_seq_type(4))
+    s1 = list(range(0,4))
+    s2 = list(range(4,8))
+    s1 += s2 
+    for i in range(0, 8):
+        s1[i] == i
+
+def test_multiplication():
     pass
 
-test_addition_assignment():
-    pass
-
-test_multiplication():
-    pass
-
-test_multiplication_assignment():
+def test_multiplication_assignment():
     pass
