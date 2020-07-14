e65cf8bf4e9296b877cb1a31084bf185eda3c458
hman523 <shbarbella@gmail.com>
2020-07-14 15:41:56 -0400

    Finished the test

--- test/python/test_vector_operators.py
+++ test/python/test_vector_operators.py
@@ -1,32 +1,30 @@
 import rti.connextdds as dds
 
 
-
-
-
-def get_seq_type(size):
-    return dds.SequenceType(dds.Int32Type(), size)
-
 def test_addition():
-    s1 = dds.DynamicData(get_seq_type(4))
-    s2 = dds.DynamicData(get_seq_type(4))
-    s1 = list(range(0,4))
-    s2 = list(range(4,8))
+    s1 = dds.Int32Seq(range(0,4))
+    s2 = dds.Int32Seq(range(4,8))
     s3 = s1 + s2
     for i in range(0, 8):
         assert s3[i] == i
 
 def test_addition_assignment():
-    s1 = dds.DynamicData(get_seq_type(4))
-    s2 = dds.DynamicData(get_seq_type(4))
-    s1 = list(range(0,4))
-    s2 = list(range(4,8))
+    s1 = dds.Int32Seq(range(0,4))
+    s2 = dds.Int32Seq(range(4,8))
     s1 += s2 
     for i in range(0, 8):
-        s1[i] == i
+        assert s1[i] == i
 
 def test_multiplication():
-    pass
+    s1 = dds.Int32Seq(range(0, 4))
+    s2 = s1*2
+    for i in range(0,4):
+        assert s2[i] == i
+        assert s2[i+4] == i
 
 def test_multiplication_assignment():
-    pass
+    s1 = dds.Int32Seq(range(0,4))
+    s1 *= 2
+    for i in range(0,4):
+        assert s1[i] == i
+        assert s1[i+4] == i
